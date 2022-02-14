3a066be5cd939dd000055d939999d2119831552e
sliatecinos <sliatecinos@hotmail.com>
2022-02-14 00:12:27 -0300

    Update readthedocs version

--- br_registrations/version.py
+++ br_registrations/version.py
@@ -2,9 +2,9 @@
 """
 Author       : sliatecinos
 Created Time : 2021-01-19 21:59:00
-Updated Time : 2021-02-11 02:00:00
+Updated Time : 2021-02-13 23:11:00
 BR Registrations
 environments:
 
 """
-__version__ = '0.0.0.8'
+__version__ = '0.0.0.9'
