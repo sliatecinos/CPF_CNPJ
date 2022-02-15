fea77079c6bc7c2bfaa64d817c5437773d6d384e
sliatecinos <sliatecinos@hotmail.com>
2022-02-15 16:56:40 -0300

    Update "rtd" files

---
+++ br_registrations.py
@@ -0,0 +1,28 @@
+"""
+BR registrations - Biblioteca Python usada para validação de números de CNPJ/CPF usados no Brasil.
+"""
+
+__version__ = "0.0.0.9"
+
+
+class CNPJ():
+    """Traz os metodos de validação de CNPJ."""
+    pass
+
+
+class CPF():
+    """Traz os metodos de validação de CPF."""
+    pass
+
+
+def version():
+    """
+    retorna a versão atual da instalação de br-registrations.
+
+    :param: N/A.
+    :type: N/A.
+    :raise lumache.invalidkinderror: if the kind is invalid.
+    :return: versão da lib.
+    :rtype: str
+    """
+    return '0.0.0.9'
