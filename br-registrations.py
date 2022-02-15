1a20fce859086272f1fd65854b5dafb717276b32
sliatecinos <sliatecinos@hotmail.com>
2022-02-15 14:26:07 -0300

    Update "rtd" docs

---
+++ br-registrations.py
@@ -0,0 +1,23 @@
+"""
+BR registrations - Biblioteca Python usada para validação de números de CNPJ/CPF usados no Brasil.
+"""
+
+__version__ = "0.0.0.9"
+
+
+class InvalidKindError(Exception):
+    """Raised if the kind is invalid."""
+    pass
+
+
+def get_random_ingredients(kind=None):
+    """
+    Return a list of random ingredients as strings.
+
+    :param kind: Optional "kind" of ingredients.
+    :type kind: list[str] or None
+    :raise lumache.InvalidKindError: If the kind is invalid.
+    :return: The ingredients list.
+    :rtype: list[str]
+    """
+    return ["shells", "gorgonzola", "parsley"]
