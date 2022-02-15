35eaea392fecb9ed4648b6e03fb07e638d8426ad
sliatecinos <sliatecinos@hotmail.com>
2022-02-15 18:06:56 -0300

    Update multiple files

--- br_registrations/__init__.py
+++ br_registrations/__init__.py
@@ -2,6 +2,6 @@ from . import validateCNPJ
 from . import validateCPF
 
 
-from pkg_resources import get_distribution
+# from pkg_resources import get_distribution
 
-__version__ = get_distribution('br_registrations').version
+# __version__ = get_distribution('br_registrations').version
